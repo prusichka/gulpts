6f1feac83c89d463fb095d97f0b57d3542cebc20
Your Name <you@example.com>
2021-09-30 18:14:46 +0300

    zcx

--- app/script/main.js
+++ app/script/main.js
@@ -11,10 +11,10 @@ new Swiper(".slider1", {
     modifier: 1,
     slideShadows: true,
   },
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  // },
 });
 
 new Swiper (".slider2", {
@@ -30,8 +30,37 @@ new Swiper (".slider2", {
     modifier: 1,
     slideShadows: true,
   },
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  // },
+})
+new Swiper('.slider3', {
+  loop: true,
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
   },
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  // },
+})
+
+const body = document.querySelector('body');
+const openMenu = document.getElementById('open');
+const closeMenu = document.getElementById('close');
+
+openMenu.addEventListener('click', ()=> {
+  body.classList.add('lock-scroll');
+})
+closeMenu.addEventListener('click', ()=> {
+  body.classList.remove('lock-scroll');
 })
